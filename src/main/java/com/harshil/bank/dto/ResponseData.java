8a31708ef7862f347168b284366dff44f115cea3
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2026-03-26 23:59:22 +0530

    added getter/setter and new fields

--- src/main/java/com/harshil/bank/dto/ResponseData.java
+++ src/main/java/com/harshil/bank/dto/ResponseData.java
@@ -2,11 +2,47 @@ package com.harshil.bank.dto;
 
 public class ResponseData{
 
-    public String message;
-    public int status;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
 
-    public ResponseData(String message,int status){
-        this.message = message;
-        this.status = status;
+    public ResponseData(){}
+
+    // setters
+
+    public void setName(String name){
+        this.name = name;
     }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    // getters
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
 }
