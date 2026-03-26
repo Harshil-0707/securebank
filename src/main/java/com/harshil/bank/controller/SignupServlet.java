51c72c03b78ddb8f45df7b97410b15f67e79c3aa
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2026-03-27 00:00:01 +0530

    added post request feature

--- src/main/java/com/harshil/bank/controller/SignupServlet.java
+++ src/main/java/com/harshil/bank/controller/SignupServlet.java
@@ -5,14 +5,40 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import java.io.*;
 
+import com.harshil.bank.dto.ResponseData;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebServlet("/signup")
 public class SignupServlet extends HttpServlet{
-     protected void doGet(HttpServletRequest req, HttpServletResponse res)
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
         res.setContentType("text/html");
         req.getRequestDispatcher("/signup.html").forward(req,res);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req,HttpServletResponse res)
+            throws ServletException , IOException {
+
+        BufferedReader reader = req.getReader();
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+
+        while((line = reader.readLine()) != null ){
+            sb.append(line);
+        }
+
+        String json = sb.toString();
+
+        ResponseData data = SignupServlet.mapper.readValue(json,ResponseData.class);
 
     }
 }
