22b1ada0d838a65f64bc9974dee1e9d9f447f0af
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2026-04-02 23:48:40 +0530

    added new file to clean up jdbc driver listener

---
+++ src/main/java/com/harshil/bank/util/MySQLCleanupListener.java
@@ -0,0 +1,42 @@
+package com.harshil.bank.util;
+
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.annotation.WebListener;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
+
+@WebListener
+public class MySQLCleanupListener implements ServletContextListener {
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        try {
+            // Stop MySQL cleanup thread
+            AbandonedConnectionCleanupThread.checkedShutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Unregister JDBC drivers
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+            try {
+                DriverManager.deregisterDriver(driver);
+                System.out.println("Deregistered driver: " + driver);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        // nothing needed
+    }
+}
