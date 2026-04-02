71869e1cc0c079f2740b72fb9591554f5df5a1b5
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2026-04-02 23:38:41 +0530

    added response message for login

--- src/main/java/com/harshil/bank/controller/HomeServlet.java
+++ src/main/java/com/harshil/bank/controller/HomeServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.annotation.*;
 import java.io.*;
 
 import com.harshil.bank.dto.LoginData;
+import com.harshil.bank.service.BankService;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -37,13 +38,20 @@ public class HomeServlet extends HttpServlet {
 
         String json = sb.toString();
 
-        LoginData data = HomeServlet.mapper.readValue(json,LoginData.class);
+        LoginData ld = HomeServlet.mapper.readValue(json,LoginData.class);
+
+        BankService bs = new BankService();
 
         res.setContentType("application/json");
         res.setCharacterEncoding("UTF-8");
 
         PrintWriter out = res.getWriter();
-        out.print("{\"message\":\"Success\"}");
+
+        if(bs.UserExists(ld)){
+            out.print("{\"message\":\"Success\"}");
+        }else{
+            out.print("{\"message\":\"Failed\"}");
+        }
         out.flush();
 
     }
