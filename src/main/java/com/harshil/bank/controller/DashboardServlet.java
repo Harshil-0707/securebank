3370b9e83a61adca5f61ff49f6e934d02db6a0bc
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2026-04-10 23:28:27 +0530

    used new package from jackson

--- src/main/java/com/harshil/bank/controller/DashboardServlet.java
+++ src/main/java/com/harshil/bank/controller/DashboardServlet.java
@@ -5,6 +5,10 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import java.io.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import java.math.BigDecimal;
 
 import com.harshil.bank.dto.DashboardData;
@@ -19,6 +23,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class DashboardServlet extends HttpServlet{
 
     private static final ObjectMapper mapper = new ObjectMapper();
+
+    static {
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
